aa42fe49c5ef5e6728745544f635fb27ef30d37c
dheeraj <gumudavellidheeraj@gmail.com>
2021-01-06 16:47:58 +0530

    changes

--- index.js
+++ index.js
@@ -15,8 +15,8 @@ const MONGO_URI = process.env.MONGO_URI;
 if (process.env.NODE_ENV === 'production') {
   app.use('/', express.static(path.join(__dirname, 'recipefrontend', 'build')));
 
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'recipefrontend', 'build', 'index.html'));
+  app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'recipefrontend', 'build', 'index.html'));
   })
 }
 
